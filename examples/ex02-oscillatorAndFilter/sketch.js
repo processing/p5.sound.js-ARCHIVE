7ea6dc415fd5c0bfee167e38b30eeb94dc08f622
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-07-19 15:04:01 -0400

    change name of example

---
+++ examples/ex02-oscillatorAndFilter/sketch.js
@@ -0,0 +1,45 @@
+// print the current p5sound version
+console.log(p5sound.VERSION);
+
+// declare variable for oscillator
+let osc;
+
+// declare variable for filter
+let filter;
+
+// retrieve the buttons from the DOM
+let startAudioButton = document.getElementById('startAudioButton');
+let stopAudioButton = document.getElementById('stopAudioButton');
+let startOscillatorButton = document.getElementById('startOscillatorButton');
+
+startAudioButton.addEventListener('click', function () {
+  getAudioContext().resume();
+});
+
+stopAudioButton.addEventListener('click', function () {
+  getAudioContext().suspend();
+});
+
+startOscillatorButton.addEventListener('click', function () {
+  osc.start();
+});
+
+startFilterButton.addEventListener('click', function () {
+  osc.start();
+});
+
+function setup() {
+  createCanvas(400, 400);
+  background(196);
+  osc = new Oscillator('sine', 400);
+}
+
+function draw() {
+  for (let i = 0; i < width; i++) {
+    noFill(); stroke(255*Math.random(), 16);
+    ellipse(width * Math.random(), height - i, 10, 10);
+  }
+  let newFreq = map(mouseX, 0, width, 100, 1000);
+  osc.freq(newFreq);
+  console.log(osc.getFreq());
+}

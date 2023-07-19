00b08ca53656ada4a05869e67bc345a599cda189
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-07-19 15:04:16 -0400

    add prototypes to p5

--- src/app.js
+++ src/app.js
@@ -7,9 +7,25 @@ import { getAudioContext } from './audioContext';
 p5.prototype.getAudioContext = getAudioContext;
 import p5sound from './main';
 import Oscillator from './p5sound.Oscillator';
-p5.prototype.Oscillator  = Oscillator;
+p5.prototype.Oscillator = Oscillator;
+import { SinOsc, TriOsc, SawOsc, SqrOsc } from './p5sound.Oscillator';
+p5.prototype.SinOsc = SinOsc;
+p5.prototype.TriOsc = TriOsc;
+p5.prototype.SawOsc = SawOsc;
+p5.prototype.SqrOsc = SqrOsc;
+
 import SoundFile from './p5sound.SoundFile';
 p5.prototype.SoundFile = SoundFile;
+import Effect from './p5sound.Effect';
+p5.prototype.Effect = Effect;
+import Filter from './p5sound.Filter';
+p5.prototype.Filter = Filter;
+import { LowPass, HighPass, BandPass } from './p5sound.Filter';
+p5.prototype.LowPass = LowPass;
+p5.prototype.HighPass = HighPass;
+p5.prototype.BandPass = BandPass;
+import Delay from './p5sound.Delay';
+p5.prototype.Delay = Delay;
 
 // helpers
 import { _checkFileFormats } from './helpers';

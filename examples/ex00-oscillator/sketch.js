50b6fb704b2f836e00465e3018382028db85582a
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-07-19 15:02:57 -0400

    update example for different oscillator types

--- examples/ex00-oscillator/sketch.js
+++ examples/ex00-oscillator/sketch.js
@@ -8,6 +8,10 @@ let osc;
 let startAudioButton = document.getElementById('startAudioButton');
 let stopAudioButton = document.getElementById('stopAudioButton');
 let startOscillatorButton = document.getElementById('startOscillatorButton');
+let oscSineButton = document.getElementById('oscSineButton');
+let oscTriButton = document.getElementById('oscTriButton');
+let oscSquareButton = document.getElementById('oscSquareButton');
+let oscSawButton = document.getElementById('oscSawButton');
 
 startAudioButton.addEventListener('click', function () {
   getAudioContext().resume();
@@ -20,6 +24,32 @@ stopAudioButton.addEventListener('click', function () {
 startOscillatorButton.addEventListener('click', function () {
   osc.start();
 });
+
+oscSineButton.addEventListener('click', function () {
+  // osc = new SinOsc(440);
+  osc.setType('sine');
+});
+
+oscTriButton.addEventListener('click', function () {
+  // osc = new TriOsc(440);
+  osc.setType('triangle');
+});
+
+oscSquareButton.addEventListener('click', function () {
+  // osc = new SqrOsc(440);
+  osc.setType('square');
+});
+
+oscSawButton.addEventListener('click', function () {
+  // osc = new SawOsc(440);
+  osc.setType('sawtooth');
+});
+
+
+
+
+
+
 
 function setup() {
   createCanvas(400, 400);
@@ -34,8 +64,5 @@ function draw() {
   }
   let newFreq = map(mouseX, 0, width, 100, 1000);
   osc.freq(newFreq);
-}
-
-function stopOscillator(){
-  osc.stop();
+  // console.log(osc.getFreq());
 }

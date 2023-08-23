ff1632acddfe5317a0493378e8c367721d4abfc3
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-08-23 11:49:54 -0400

    adapt code for mic input

--- examples/micInput/sketch.js
+++ examples/micInput/sketch.js
@@ -13,3 +13,29 @@ startAudioButton.addEventListener('click', function () {
 stopAudioButton.addEventListener('click', function () {
   getAudioContext().suspend();
 });
+
+let mic;
+
+function setup() {
+  createCanvas(710, 200);
+
+  // Create an Audio input
+  mic = new AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
+}
+
+function draw() {
+  background(200);
+
+  // Get the overall volume (between 0 and 1.0)
+  let vol = mic.getLevel();
+  fill(127);
+  stroke(0);
+
+  // Draw an ellipse with height based on volume
+  let h = map(vol, 0, 1, height, 0);
+  ellipse(width / 2, h - 25, 50, 50);
+}

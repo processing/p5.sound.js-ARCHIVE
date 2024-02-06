7aec508c79a938cffb128e51298113beaf82ebdf
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-02-06 17:50:56 -0300

    add placeholder file in utils for developing

--- src/audioContext.js
+++ src/audioContext.js
@@ -1,36 +1,46 @@
-// import { Tone } from 'tone';
+// import Tone.js dependency
 import * as Tone from 'tone';
+
+// create a new AudioContext from Web Audio API
 const audioContext = new window.AudioContext();
+
+// set the context for Tone.js to be the same as p5.sound.js
 Tone.setContext(audioContext);
 
 /**
  * <p>Returns the Audio Context for this sketch. Useful for users
  * who would like to dig deeper into the <a target='_blank' href=
  * 'http://webaudio.github.io/web-audio-api/'>Web Audio API
- * </a>.</p>
+ * </a>. and the reference for it at <a target='_blank' href=
+ * 'https://developer.mozilla.org/en-US/docs/Web/API/AudioContext'>
+ * MDN Web Docs</a></p>
  *
- * <p>Some browsers require users to startAudioContext
+ * <p>Browsers require users to startAudioContext
  * with a user gesture, such as touchStarted in the example below.</p>
  *
  * @for p5sound
  * @method getAudioContext
- * @return {Object}    AudioContext for this sketch
+ * @return {Object}    audioContext for this sketch
  * @example
  *  function draw() {
  *    background(255);
  *    textAlign(CENTER);
  *
- *    if (getAudioContext().state !== 'running') {
- *      text('click to start audio', width/2, height/2);
- *    } else {
- *      text('audio is enabled', width/2, height/2);
+ *    if (getAudioContext().state == 'running') {
+ *      text('audioContext is running', width/2, height/2);
+ *    } else if (getAudioContext().state == 'suspended') {
+ *      text('audio is suspended', width/2, height/2);
+ *    } else if (getAudioContext().state == 'closed') {
+ *     text('audioContext is closed', width/2, height/2);
  *    }
- *  }
  *
- *  function touchStarted() {
- *    if (getAudioContext().state !== 'running') {
+ *   text('tap or mouse press to change behavior', width/2, height/2 + 20);
+ *
+ *  function mousePressed() {
+ *    if (getAudioContext().state == 'suspended') {
  *      getAudioContext().resume();
- *    }
+ *    } else if (getAudioContext().state == 'running') {
+ *     getAudioContext().suspend();
  *  }
  *
  */
